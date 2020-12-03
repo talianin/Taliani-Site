f35a3e2a68318e81c253fe7ec1f921a0da043ea9
Nicholas Taliani <nicholastaliani@Nicholass-MacBook-Pro.local>
2020-12-03 13:33:28 -0500

    buttons edited

--- src/js/cms-preview-templates/home.js
+++ src/js/cms-preview-templates/home.js
@@ -36,7 +36,7 @@ export default class PostPreview extends React.Component {
             </div>
 
             <div className="tc">
-              <a href="#" className="btn raise">See all products</a>
+              <a href="#" className="btn raise">More About Exercise</a>
             </div>
           </div>
         </div>
@@ -57,7 +57,7 @@ export default class PostPreview extends React.Component {
             </div>
 
             <div className="tc">
-              <a href="{{.buttonLink}}" className="btn raise">Read more</a>
+              <a href="{{.buttonLink}}" className="btn raise">All About Diet</a>
             </div>
 
           </div>
